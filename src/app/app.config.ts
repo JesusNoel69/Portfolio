0b1e065e3b037759ba18f03e55721aff40e49aa6
Jesus <jnoel883@gmail.com>
2024-08-15 18:36:49 -0600

    Módulo para manejar animaciones importado .

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,9 +4,12 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
+
 
 //IMPORTANTE: se debe proporcionar el ProvideHttpCLient() para maneja peticiones http
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideClientHydration(),
-  provideHttpClient(withFetch())]
+  provideHttpClient(withFetch()), provideAnimations(), provideAnimationsAsync()]
 };
