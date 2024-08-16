2b68b41ea7420e95df316119114fd6474b843038
Jesus <jnoel883@gmail.com>
2024-08-15 18:22:37 -0600

    Orden de los componentes de la página actualizado.

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { AboutComponent } from '../about/about.component';
 import { ContactComponent } from '../contact/contact.component';
 import { EducationComponent } from '../education/education.component';
-import { LaptopComponent } from '../laptop/laptop.component';
 import { ProjectsComponent } from '../projects/projects.component';
 import { SkillsComponent } from '../skills/skills.component';
 import { HttpClientModule } from '@angular/common/http';
+import { FadeInDirective } from '../directives/fade-in.directive';
 
 
 @Component({
@@ -14,10 +14,10 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [AboutComponent,
     ContactComponent,
     EducationComponent,
-    LaptopComponent,
     ProjectsComponent,
     SkillsComponent,
-  HttpClientModule],
+    HttpClientModule,
+    FadeInDirective],
   templateUrl: './body.component.html',
   styleUrl: './body.component.css'
 })
